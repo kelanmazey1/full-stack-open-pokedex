2c36031e41269cdc80edd81fd77b62396db336e6
kelanmazey1 <kelanmazey1@gmail.com>
2021-01-04 21:15:34 +0000

    testing Hello World workflow

--- src/PokemonAbility.jsx
+++ src/PokemonAbility.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+// adding a change to test job
 const PokemonAbility = ({ abilityName }) => (
   <div className="pokemon-ability">
     <div className="pokemon-ability-type">Hidden ability</div>
