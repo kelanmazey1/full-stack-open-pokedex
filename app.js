fb0a712d12b43d580c16b2f1310204774f4ed3f1
kelanmazey1 <kelanmazey1@gmail.com>
2021-01-07 11:23:30 +0000

    testing adding notifications to workflow

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.get('/version', (req, res) => {
-  res.send('1')
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
